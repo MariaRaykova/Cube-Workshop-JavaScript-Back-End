3833123c233a4bff6904520a51bf9ed93bf71b4f
Maria Raykova <mimi.raykova@gmail.com>
2020-11-19 22:33:29 +0200

    feat: serve dynamic data

--- controllers/create-cube.js
+++
@@ -1,5 +0,0 @@
-const Cube = require('../models/cube');
-
-// const newCube1 = new Cube('3d Gear Cube', 'Gear meshing cube is certainly unique in operation as compared to the rest.  The cube is also available at an affordable cost and is made from nontoxic, durable plastic.', 'http://wonderfulengineering.com/wp-content/uploads/2016/03/10-Best-Rubiks-cubes-4.jpg', 5);
-
-// newCube5.save();
