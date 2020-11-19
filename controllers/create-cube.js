afc70959433701e4a8a1f6d881cd83d448c4000a
Maria Raykova <mimi.raykova@gmail.com>
2020-11-19 22:29:01 +0200

    feat: serve dynamic data

--- controllers/create-cube.js
+++ controllers/create-cube.js
@@ -1,7 +1,5 @@
 const Cube = require('../models/cube');
 
-const newCube = new Cube('Default', 'This is a default cube', 'http://google.com', 1);
+// const newCube1 = new Cube('3d Gear Cube', 'Gear meshing cube is certainly unique in operation as compared to the rest.  The cube is also available at an affordable cost and is made from nontoxic, durable plastic.', 'http://wonderfulengineering.com/wp-content/uploads/2016/03/10-Best-Rubiks-cubes-4.jpg', 5);
 
-console.log(newCube);
-
-newCube.save();
+// newCube5.save();
--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,46 @@
 
-const {Router} = require('express');
+const { Router } = require('express');
+const cubes = require('../controllers/cubes');
+const { getCube } = require('../controllers/database')
+const { getAllCubes } = require('../controllers/cubes')
 
-    const router = Router(); 
+const router = Router();
 
-     router.get('/', function (req, res) { 
-      res.render('index', {
-        title: 'Cube Workshop'
-      }); 
-    })
+router.get('/', function (req, res) {
+  getAllCubes((cubes) => {
+    res.render('index', {
+      title: 'Cube Workshop',
+      cubes
+    });
+  });
+})
 
-    router.get('/about', function (req, res) { 
-      res.render('about', {
-        title: 'About | Cube Workshop'
-      }); 
-    })
+router.get('/about', function (req, res) {
+  res.render('about', {
+    title: 'About | Cube Workshop'
+  });
+})
 
-    router.get('/create', function (req, res) { 
-      res.render('create', {
-        title: 'Create Cube | Cube Workshop'
-      }); 
-    })
+router.get('/create', function (req, res) {
+  res.render('create', {
+    title: 'Create Cube | Cube Workshop'
+  });
+})
 
-    router.get('/details/:id', function (req, res) { 
-      res.render('details', {
-        title: 'Details | Cube Workshop'
-      }); 
-    })
+router.get('/details/:id', (req, res) => {
 
-    router.get('*', function (req, res) {  
-      res.render('404', {
-        title: 'Cube Workshop'
-      }); 
-    })
-    module.exports = router; 
+  getCube(req.params.id, (cube) => {
+
+    res.render('details', {
+      title: 'Details | Cube Workshop',
+      ...cube 
+    });
+  });
+});
+
+router.get('*', function (req, res) {
+  res.render('404', {
+    title: 'Cube Workshop'
+  });
+})
+module.exports = router; 
